6af7ea375192d9ed741dc2e25dd7ab5607d6bcc5
kobayashitorao111 <kobayasitorao111@gmail.com>
2024-02-15 17:26:21 +1000

    complete 1061

--- __mocks__/react-native-device-info.js
+++
@@ -1,8 +0,0 @@
-const DeviceInfo = {
-  getVersion: jest.fn(() => '1.0.0'),
-  getModel: jest.fn(() => 'iPhone X'),
-  getBuildNumber: jest.fn(() => '1.13.0'),
-  // Add other mocked methods and properties as needed
-};
-
-export default DeviceInfo;
